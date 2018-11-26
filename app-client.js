a812183155f1b4941e108e38462d7a67734f23e7
Quintessa <anderson.quintessa@gmail.com>
2018-11-25 21:35:24 -0600

    pushing code changes to app-client.js, syntax error still for react render function

--- app-client.js
+++ app-client.js
@@ -1,8 +1,15 @@
-var React = require('react');
-var APP = require('./components/app.js');
+// var React = require('react');
+// var APP = require('./components/app.js');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import APP from './components/app.js';
 
 //render the APP component into React Container
-React.render(<APP/>, document.getElementById('react-container'));
+ReactDOM.render(
+  <APP/>,
+  document.getElementById("react-container")
+);
 
 //Next step: convert JSX code and ES6 to ES5 or below so the browser understands render function
 //compile app.js and app-client.js to bundle.js and server it to the browser
