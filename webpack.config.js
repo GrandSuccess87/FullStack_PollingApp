54ae50a361f178b5c625b768de9bbfbbd2ef7600
Quintessa <anderson.quintessa@gmail.com>
2018-11-25 21:21:16 -0600

    pushing code for webpack configuration, bundle.js build successful, fixing syntax error app-client.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 //config info for webpack
+const path = require('path');
 
 module.exports = {
-  entry: "./app-client.js",
+  mode: "none",
+  entry: path.resolve(__dirname, "./app-client.js"),
   output: {
+    path: path.resolve(__dirname, "dist"),
     filename: "public/bundle.js"
   },
   module: {
-    mode: none,
     rules: [
       {
         exclude:/(node_modules|app-server.js)/,
-        loader: 'babel'
+        use: [
+          {loader: "babel-loader"},
+          // {
+          //   loader: "eslint-loader",
+            // options: {
+            //   formatter: require("eslint/lib/formatters/stylish")
+            // }
+          // }
+        ]
       }
     ]
   }
